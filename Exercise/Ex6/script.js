22755ad2f401d5438036dd31ef3976f6040c5739
codeaid-dev <codeaid.lab@gmail.com>
2025-10-01 22:46:29 +0900

    modify

---
+++ Exercise/Ex6/script.js
@@ -0,0 +1,16 @@
+const btns = document.querySelectorAll('button');
+const disc = document.querySelector('#discription');
+for (let i=0; i<btns.length; i++) {
+  btns[i].addEventListener('click', ()=>{
+    if (btns[i].id == 'small') {
+      // disc.setAttribute('style', 'font-size:14px;');
+      disc.style.fontSize = '14px';
+    } else if (btns[i].id == 'medium') {
+      // disc.setAttribute('style', 'font-size:16px;');
+      disc.style.fontSize = '16px';
+    } else if (btns[i].id == 'large') {
+      // disc.setAttribute('style', 'font-size:20px;');
+      disc.style.fontSize = '20px';
+    }
+  });
+}
