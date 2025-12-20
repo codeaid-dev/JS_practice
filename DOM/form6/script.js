f2653278e3b94945fbabb9caaebc15df89f0fd45
codeaid-dev <codeaid.lab@gmail.com>
2025-12-20 09:23:07 +0900

    add

---
+++ DOM/form6/script.js
@@ -0,0 +1,16 @@
+const up = document.querySelector('[name="up"]');
+const ftype = document.querySelectorAll('[name="file-type"]');
+ftype.forEach((element) => {
+  element.addEventListener('change', (event) => {
+    if (element.checked) {
+      up.accept = element.value;
+    }
+  });
+});
+// up.addEventListener('change', (event) => {
+//   const file = event.currentTarget.files[0];
+//   const fname = file.name;
+//   const fsize = file.size;
+//   const mime = file.type;
+//   console.log(`file name: ${fname}, file size: ${fsize}, MIME type: ${mime}`);
+// });
