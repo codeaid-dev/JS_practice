762851ba5ff893792c9f5c07723368a92528338a
codeaid-dev <codeaid.lab@gmail.com>
2026-01-05 10:27:55 +0900

    add

---
+++ DOM/setTimeout/script.js
@@ -0,0 +1,14 @@
+let timerId;
+
+const start = document.getElementById("start");
+start.addEventListener('click', () => {
+  timerId = setTimeout(() => {
+    alert("5秒経過しました");
+  }, 5000);
+});
+
+const cancel = document.getElementById("cancel");
+cancel.addEventListener('click', () => {
+  clearTimeout(timerId);
+  alert("キャンセルしました");
+});
