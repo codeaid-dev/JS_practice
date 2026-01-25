11c3a5a1a5fb5c2ad53b19de324d6c3150cd044a
codeaid-dev <codeaid.lab@gmail.com>
2026-01-25 11:17:35 +0900

    add

--- Drill/Ex39/script.js
+++ Drill/Ex39/script.js
@@ -1,63 +1,25 @@
-  const state = {
-    step: 1,
-    name: '',
-    email: ''
-  };
+const checks = document.querySelectorAll('input[type="checkbox"]');
+const items = document.querySelectorAll('#items li');
 
-  const steps = document.querySelectorAll('.step');
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
-  const confirm = document.getElementById('confirm');
-  const next = document.getElementById('next');
-  const prev = document.getElementById('prev');
-  const error = document.getElementById('error');
+checks.forEach((cb) => {
+  cb.addEventListener('change', updateFilter);
+});
+updateFilter();
 
-  function render() {
-    steps.forEach(step => {
-      step.classList.toggle(
-        'active',
-        Number(step.dataset.step) === state.step
-      );
-      next.textContent = state.step === 3 ? '確認' : '次へ';
-      if (state.step === 1) prev.style.display = 'none';
-      else prev.style.display = 'inline';
-    });
-
-    name.value = state.name;
-    email.value = state.email;
-    confirm.textContent = `
-      名前: ${state.name}
-      メール: ${state.email}
-    `;
-  }
-
-  name.addEventListener('input', (event) => {
-    state.name = event.target.value;
+function updateFilter() {
+  const checkTypes = [...checks].filter((cb) => cb.checked);
+  const activeTypes = [];
+  checkTypes.forEach((cb) => {
+    activeTypes.push(cb.value);
   });
+  // const activeTypes = [...checks].filter(cb => cb.checked).map(cb => cb.value);
 
-  email.addEventListener('input', (event) => {
-    state.email = event.target.value;
+  items.forEach(item => {
+    const type = item.dataset.type;
+    if (activeTypes.includes(type))
+      item.style.display = 'list-item';
+    else
+      item.style.display = 'none';
+    // item.style.display = activeTypes.includes(type) ? 'list-item' : 'none';
   });
-
-  next.addEventListener('click', () => {
-    if (state.step < 3) {
-      if (state.step === 1 && name.value.length === 0) {
-        error.textContent = '入力してください';
-      } else if (state.step === 2 && email.value.length === 0) {
-        error.textContent = '入力してください';
-      } else {
-        state.step++;
-        error.textContent = '';
-      }
-    }
-    render();
-  });
-
-  prev.addEventListener('click', () => {
-    if (state.step > 1) {
-      state.step--;
-    }
-    render();
-  });
-
-  render();
+}
