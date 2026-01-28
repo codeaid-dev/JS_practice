ad9dfa2256ba9231577d2d9edef891f4ddaba5d1
codeaid-dev <codeaid.lab@gmail.com>
2026-01-28 09:10:59 +0900

    fix

--- Drill/Ex40/script.js
+++ Drill/Ex40/script.js
@@ -1,31 +1,31 @@
 const tiles = document.querySelectorAll('.tile');
 
-// tiles.forEach((tile, i) => {
-//   tile.addEventListener('click', () => {
-//     if (tile.style.background === 'red') {
-//       tile.style.background = null;
-//       tile.innerText = '';
-//     } else if (tiles[i].style.background === 'blue') {
-//       tile.style.background = 'red';
-//       tile.innerText = Number(i)+1;
-//     } else {
-//       tile.style = 'background:blue';
-//       tile.innerText = Number(i)+1;
-//     }
-//   });
-// });
-
-for (let tile of tiles) {
-  tile.addEventListener('click', (event) => {
+tiles.forEach((tile, i) => {
+  tile.addEventListener('click', () => {
     if (tile.style.background === 'red') {
       tile.style.background = null;
       tile.innerText = '';
     } else if (tiles[i].style.background === 'blue') {
       tile.style.background = 'red';
-      tile.innerText = Array.from(tiles).indexOf(tile)+1;
+      tile.innerText = Number(i)+1;
     } else {
       tile.style = 'background:blue';
-      tile.innerText = Array.from(tiles).indexOf(tile)+1;
+      tile.innerText = Number(i)+1;
     }
   });
-}
+});
+
+// for (let tile of tiles) {
+//   tile.addEventListener('click', () => {
+//     if (tile.style.background === 'red') {
+//       tile.style.background = null;
+//       tile.innerText = '';
+//     } else if (tile.style.background === 'blue') {
+//       tile.style.background = 'red';
+//       tile.innerText = Array.from(tiles).indexOf(tile)+1;
+//     } else {
+//       tile.style = 'background:blue';
+//       tile.innerText = Array.from(tiles).indexOf(tile)+1;
+//     }
+//   });
+// }
