774cd06fa33aeafefc876d24c1bb85c60dbdaf49
codeaid-dev <codeaid.lab@gmail.com>
2026-02-06 17:02:31 +0900

    fix

--- Drill/Ex47/script.js
+++ Drill/Ex47/script.js
@@ -1,36 +1,38 @@
 const disp = document.querySelector('#display');
 const keys = document.querySelectorAll('button');
-let total = 0;
-let numStat = true; // true:演算子入力後、false:数値入力後
-let calc = '+';
-let num = '';
+let total = '0';
+let start = true; // true:演算開始、false:演算中
 
+disp.innerText = total;
 for (let i=0; i<keys.length; i++) {
   keys[i].addEventListener('click', () => {
     let val = keys[i].value;
-    if (!isNaN(val) || val === '.') {
-      numStat = false;
-      num += val;
-      disp.innerText = num;
-    } else if (val === 'C') {
-        num = '';
-        total = 0;
-        calc = '+';
-        disp.innerText = '';
-    } else {
-      if (!numStat) {
-        numStat = true;
-        console.log(total + calc + num);
-        total = eval(total + calc + num);
-        num = '';
-        disp.innerText = total;
-      }
-      if (val === '=') {
-        total = 0;
-        calc = '+';
+    if (val !== 'C' && val !== '=') {
+      if (start && !isNaN(val)) {
+        total = val;
+        start = false;
+      } else if (!isNaN(val) && total === '0') {
+        total = val;
       } else {
-        calc = val;
+        if (isNaN(total[total.length-1])) {
+          if (!isNaN(val)) {
+            total += val;
+            start = false;
+          }
+        } else {
+          total += val;
+          start = false;
+        }
       }
+      disp.innerText = total;
+    } else if (val === '=') {
+      total = String(eval(total));
+      disp.innerText = total;
+      start = true;
+    } else if (val === 'C') {
+      total = '0';
+      start = true;
+      disp.innerText = total;
     }
   });
 }
