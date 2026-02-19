199532f9901a0cda83acb196c50c9e29a7b66b15
codeaid-dev <codeaid.lab@gmail.com>
2026-02-19 21:36:32 +0900

    add

---
+++ function/external_call/exporter.js
@@ -0,0 +1,11 @@
+export function say_hello() {
+  console.log('Hello');
+}
+
+export const say_goodbye = () => {
+  console.log('Good Bye');
+}
+
+export default function say_surprise() {
+  console.log('Wow!!');
+}
---
+++ function/external_call/script.js
@@ -0,0 +1,6 @@
+import surprise, { say_hello as hello, say_goodbye } from "./exporter.js";
+
+hello(); // Hello
+say_goodbye(); // Good Bye
+
+surprise(); // Wow!!
