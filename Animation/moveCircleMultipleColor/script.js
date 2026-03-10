d26a5dcb01787011f8993f12e47ca98a510b124d
codeaid-dev <codeaid.lab@gmail.com>
2026-03-10 22:52:33 +0900

    add

--- Animation/moveCircleMultipleColor/script.js
+++ Animation/moveCircleMultipleColor/script.js
@@ -4,10 +4,18 @@ canvas.width = 500;
 canvas.height = 500;
 
 const radius = 20;
-class Circle {}
+class Circle {
+  collision(circle) {
+    const distanceX = circle.x - this.x;
+    const distanceY = circle.y - this.y;
+    if (Math.sqrt(distanceX ** 2 + distanceY ** 2)
+        <= circle.radius + this.radius)
+      return true;
+    return false;
+  }
+}
 const circles = [];
 const createCircle = () => {
-  // const circle = {}
   const circle = new Circle();
   const max = canvas.width - radius;
   const min = radius;
@@ -16,6 +24,7 @@ const createCircle = () => {
   circle.dx = Math.random() * 3 + 1;
   circle.dy = Math.random() * 4 + 1;
   circle.radius = radius;
+  circle.color = '#000';
   circles.push(circle);
 }
 
@@ -25,7 +34,7 @@ const loop = () => {
   for (let circle of circles) {
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2);
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = circle.color;
     ctx.fill();
     circle.x += circle.dx;
     circle.y += circle.dy;
@@ -34,6 +43,18 @@ const loop = () => {
     }
     if (circle.y > canvas.height-circle.radius || circle.y < circle.radius) {
       circle.dy *= -1;
+    }
+
+    for (let other of circles) {
+      if (circle !== other) {
+        if (circle.collision(other)) {
+          const r = Math.random()*256;
+          const g = Math.random()*256;
+          const b = Math.random()*256;
+          circle.color = `rgb(${r} ${g} ${b})`;
+          break;
+        }
+      }
     }
   }
   requestAnimationFrame(loop);
