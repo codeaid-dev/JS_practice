5a43419c9156430548a7f481062678cc8f633d80
codeaid-dev <codeaid.lab@gmail.com>
2026-08-08 14:16:15 +0900

    fix

--- Drill/Ex51/script.js
+++ Drill/Ex51/script.js
@@ -1,17 +1,17 @@
 const words = ['apple', 'banana', 'grape', 'りんご', 'みかん', 'いちご'];
-const input = document.getElementById('input');
+const search = document.getElementById('search');
 const suggest = document.getElementById('suggest');
 
-input.addEventListener('input', () => {
+search.addEventListener('input', () => {
   suggest.innerHTML = '';
-  if (!input.value) return;
+  if (!search.value) return;
 
-  words.filter(w => w.includes(input.value))
+  words.filter(w => w.includes(search.value))
     .forEach(w => {
       const li = document.createElement('li');
       li.textContent = w;
       li.addEventListener('click', () => {
-        input.value = w;
+        search.value = w;
         suggest.innerHTML = '';
       });
       suggest.appendChild(li);
